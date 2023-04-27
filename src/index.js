3e1188b465e35fe07a554da9ae65bb511f1241d7
TienMinh25 <letienminh2512@gmail.com>
2023-04-28 00:08:18 +0700

    Remove public/css folder

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname, 'public'));
 // HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({ // config for handlebars
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => 
@@ -18,6 +23,13 @@ app.get('/', (req, res) =>
     }
 
 ); // dinh nghia route
+
+app.get('/news', (req, res) => 
+    {
+        res.render('news');
+    }
+
+); 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
  
