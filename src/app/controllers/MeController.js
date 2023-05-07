f97c3abe7856a5161596f8a1110c2ba37de78cb3
TienMinh25 <letienminh2512@gmail.com>
2023-05-07 22:59:47 +0700

    Add deleted count documents

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,10 +4,14 @@ const { multipleMongooseToObject, mongooseToObject } = require('../../util/mongo
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
-                courses: multipleMongooseToObject(courses)
-            }))
+
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) => 
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses)
+                })
+            )
             .catch(next);
     }
 
