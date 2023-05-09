c96f19c851f359a9e6175d5c714c8e249808e0b4
TienMinh25 <letienminh2512@gmail.com>
2023-05-09 09:34:10 +0700

    Sort query helper

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,14 +4,8 @@ const { multipleMongooseToObject, mongooseToObject } = require('../../util/mongo
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        let courseQuery = Course.find({});
-        if (req.query.hasOwnProperty('_sort')){
-            courseQuery = courseQuery.sort({
-                [req.query.column]: req.query.type
-            });
-        }
 
-        Promise.all([courseQuery, Course.countDocumentsDeleted()])
+        Promise.all([Course.find({}).sortable(req), Course.countDocumentsDeleted()])
             .then(([courses, deletedCount]) => 
                 res.render('me/stored-courses', {
                     deletedCount,
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -15,6 +15,18 @@ const Course = new Schema({
     timestamps: true,
 });
 
+// Custom query helpers
+Course.query.sortable = function(req) {
+    if (req.query.hasOwnProperty('_sort')){
+        const isValidType = ['asc', 'desc'].includes(req.query.type);
+
+        return this.sort({
+            [req.query.column]: isValidType ? req.query.type : 'desc',
+        });
+    }
+    return this;
+}
+
 // Add plugins
 mongoose.plugin(slug);
 Course.plugin(mongooseDelete, { 
